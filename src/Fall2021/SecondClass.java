f3e9ffb82f31e5eeb204391f791cec68cfce22bf
Mustafa Aptish <mustafaaptish@gmail.com>
2022-02-11 19:56:20 +0000

    Last updates

--- src/Fall2021/SecondClass.java
+++ src/Fall2021/SecondClass.java
@@ -5,7 +5,5 @@ public class SecondClass {
         System.out.println("new page second class");
         System.out.println("how are you");
         System.out.println("fine");
-        
-
     }
 }
